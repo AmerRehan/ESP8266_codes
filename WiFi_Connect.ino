55857c8c82e1b2cd117cf31c6e2b13ab05c60d02
SYED AMER REHAN <103924173+AmerRehan@users.noreply.github.com>
2022-06-26 09:14:30 +0530

    Update WiFi_Connect.ino

--- WiFi_Connect.ino
+++ WiFi_Connect.ino
@@ -1,12 +1,9 @@
 #include<ESP8266WiFi.h>
 
 char* ssid = "SIEMENS";   // WiFi name
-char* pass = "$i#m#n$@CAM";  // WiFi pass
+char* pass = "$i#m#n$@CAM";  // WiFi password
 
 WiFiClient client;
-
-char* api = "KIJ6VDE092IY3TTK";   // write api key
-char ip[] = "184.106.153.149";
 
 void setup() {
   Serial.begin(9600);
